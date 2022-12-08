3b225fad9165478d2e995cc61d431ff55ba572cc
Asya İlktürk <asya.ilkturk@gmail.com>
2022-12-08 04:07:29 +0300

    .

--- src/Services/Report/Report.Api/Service/GoogleSheetService.cs
+++ src/Services/Report/Report.Api/Service/GoogleSheetService.cs
@@ -9,8 +9,8 @@ namespace Report.API.Service
 {
     public class GoogleSheetService : IGoogleSheetService
     {
-        const string SPREADSHEET_ID = "11qqVJ0rZXONJP-QCBxs5S_u5DuxIG8dctRCAPDPsJJE";
-        const string SHEET_NAME = "Reports";
+        //const string SPREADSHEET_ID = "11qqVJ0rZXONJP-QCBxs5S_u5DuxIG8dctRCAPDPsJJE";
+        //const string SHEET_NAME = "Reports";
         SpreadsheetsResource _googleSheetService;
         
 
@@ -18,17 +18,17 @@ namespace Report.API.Service
         {
             _googleSheetService = googleSheetsHelper.Service.Spreadsheets;
         }
-        public List<ReportData> GetDatas()
+        public List<ReportData> GetData(string spreadSheetId)
         {
             var range = "A:C";
-            var request = _googleSheetService.Values.Get(SPREADSHEET_ID, range);
+            var request = _googleSheetService.Values.Get(spreadSheetId, range);
             
             var response = request.Execute();
             var values = response.Values;
             return ReportMapper.MapFromRangeData(values);
         }
 
-        public void AddDatas(List<ReportData> reports)
+        public void AddDatas(List<ReportData> reports, string spreadSheetId)
         {
             var range = "A:C";
             var valueRange = new ValueRange
@@ -36,7 +36,7 @@ namespace Report.API.Service
                 Values = ReportMapper.MapToRangeData(reports)
             };
 
-            var appendRequest = _googleSheetService.Values.Append(valueRange, SPREADSHEET_ID, range);
+            var appendRequest = _googleSheetService.Values.Append(valueRange, spreadSheetId, range);
             appendRequest.ValueInputOption = AppendRequest.ValueInputOptionEnum.USERENTERED;
             appendRequest.Execute();
         }
